9dea33d1871d2b1f808f17052ce6f4660637c0b1
alykang <kanga850@newschool.edu>
2024-03-27 19:36:21 -0400

    3.27

---
+++ script.js
@@ -0,0 +1,241 @@
+const bottles = [ 
+  {
+    "name": "Happy Tears Zero Sugar",
+    "year": "2024",
+    "material": "Aluminum",
+    "fact": "This coke infused with a pinch of salt and peach flavor, is only sold on TikTok shop and is meant to promote Random Acts of Kindness Day. ",
+    "rating": "xxxx",
+    "img": "happytears.png",
+  },
+  {
+    "name": "Ultimate",
+    "year": "2023",
+    "material": "Plastic",
+    "fact": "Coca-Cola's collaboration with League of Legends and has the flavor of +XP. Many consumers claim that it tastes like Coke mixed with a variety of different fruits.",
+    "rating": "xxxx",
+    "img": "ultimate.png",
+  },
+  {
+    "name": "Starlight",
+    "year": "2022",
+    "material": "Plastic made with 100% recyclable materials.",
+    "fact": "Released as the first flavor from Coca-Cola Creations, an innovation platform of The Coca-Cola Company focused on selling limited-edition flavors, it provides a subtle cooling sensation with the classic Coca-Cola flavor. ",
+    "rating": "xxxx",
+    "img": "Starlight.png"
+  },
+  {
+    "name": "Japan Destination Bottle",
+    "year": "2017",
+    "material": "Aluminum",
+    "fact": "This special edition bottle can only be found in specific areas of Japan that each of the nine bottles was deesigned for.",
+    "rating": "xxxx",
+    "img": "Japan.png"
+  },
+  {
+    "name": "Skinny Can",
+    "year": "2013",
+    "material": "Aluminum",
+    "fact": "The skinny can was released as a response to the growing health-concious prefernces of consumers. By releasing sleeker, taller cans in smaller 250ml portions, Coca-Cola aimed to appeal to the masses once again.",
+    "rating": "xxxx",
+    "img": "skinny.png"
+  },
+  {
+    "name": "Share a Coke",
+    "year": "2011",
+    "material": "Plastic made with 100% recyclable materials.",
+    "fact": "The campaign originated in Australia with the goal to strengthen the connection between the company and its younger consumers as well as inspire moments of happiness amongst consumers.",
+    "rating": "xxxx",
+    "img": "share_bottle.png"
+  },
+  {
+    "name": "PlantBottle",
+    "year": "2009",
+    "material": "100% recyclable; composed of 30% plant-based materials.",
+    "fact": "Since introducing PlantBottle, Coca-Cola has allowed non-competitive companies to use the technology such as Heinz Tomato Ketchup, to the fabric interior in some Ford Fusion hybrid sedans",
+    "rating": "xxxx",
+    "img": "plant-based.png",
+  },
+  {
+    "name": "Aluminum Contour",
+    "year": "2008",
+    "material": "Aluminum",
+    "fact": "It became extremely popular because it was lighter and wouldn't break like glass bottles, had a resealable top, and was recyclable.",
+    "rating": "xxxx",
+    "img": "contourcan.png",
+  },
+  {
+    "name": "Mini",
+    "year": "2007",
+    "material": "Aluminum",
+    "fact": "This mini can was released because of the steady decline in soda consumption in past years. By making smaller cans, Coca-Cola saves money on packaging and it appeals to consumers who are looking to cut back their soda consumption.",
+    "rating": "xxxx",
+    "img": "mini.png",
+  },
+  {
+    "name": "Coca-Cola Zero",
+    "material": "Aluminum",
+    "year": "2005",
+    "fact": "Coca-Cola Zero was the company's largest new product launch in 22 years. In the U.S. it was advertised as calorie-free rather than diet because young males associated diet with women. Later in 2017, the name was changed to Coca-Cola Zero Sugar which faced a lot of backlash.",
+    "rating": "xxxx",
+    "img": "Cokezero.png",
+  },
+  {
+    "name": "Diet Coca-Cola",
+    "year": "1982",
+    "material": "Aluminum",
+    "fact": "The creation of Diet Coke  was strictly top secret and made a top priority; only a handful of senior executives knew about it, and team members had to agree to the assignment before getting the details.",
+    "rating": "xxxx",
+    "img": "diet.png",
+  },
+  {
+    "name": "2 Liter Bottle",
+    "year": "1978",
+    "material": "PET Plastic",
+    "fact": "It became extremely popular because it was lighter and wouldn't break like glass bottles, had a resealable top, and was recyclable.",
+    "rating": "xxxx",
+    "img": "plastic.png",
+  },
+  {
+    "name": "Aluminum Can",
+    "year": "1965",
+    "material": "Aluminum",
+    "fact": "Coca-Cola first tested the aluminum can in 1965 because they proved to have more benefits than steel cans. However the ubiquitous can packaging was not established until 1984.",
+    "rating": "xxxx",
+    "img": "aluminumcan.png",
+  },
+  {
+    "name": "Steel Can",
+    "year": "1955",
+    "material": "Steel",
+    "fact": "The early packaging on the steel cans had a picture of the contour bottle so people would know the same Coca-Cola they found inside a bottle was inside the can.",
+    "rating": "xxxx",
+    "img": "steelCan.png",
+  },
+  {
+    "name": "Two Color Label",
+    "year": "1961",
+    "material": "Glass bottle and Applied Color Labeling",
+    "fact": "When the ACL process became more advanced, the two-color full wrap label was developed. This label was mainly used in international markets.",
+    "rating": "xxxx",
+    "img": "twoColorbottle.png",
+  },
+  {
+    "name": "The White Label",
+    "year": "1957",
+    "material": "Glass bottle and white Applied Color Labeling",
+    "fact": "Using white Applied Color Labeling, Coca-Cola moved away from embossing the glass bottle to printing their logo on the glass",
+    "rating": "xxxx",
+    "img": "whiteLabel.png",
+  },
+  {
+    "name": "The Iconic Countour Bottle",
+    "year": "1916",
+    "material": "Glass",
+    "fact": "The patent was renewed on December 25th, 1923 and was thus coined the Christmas bottle.",
+    "rating": "xxxx",
+    "img": "christmas.png",
+  },
+  {
+    "name": "Contour Bottle",
+    "year": "1915",
+    "material": "Glass",
+    "fact": "Developed by the Root Glass Company and inspired by the cocoa bean, the prototype never made it to production because of its unstable structure.",
+    "rating": "xxxx",
+    "img": "1915contour.png",
+  },
+  {
+    "name": "Amber Bottle",
+    "year": "1906",
+    "material": "Glass",
+    "fact": "An amber version of the glass bottle was introduced to help it stand out from competitors. Unfortunately many knock off soda comapnies began to copy the bottle and take business away from Coca-Cola.",
+    "rating": "xxxx",
+    "img": "amberGlass.png",
+  },
+  {
+    "name": "Glass Bottle",
+    "year": "1900",
+    "material": "Glass",
+    "fact": "A diamond shaped bottle with a colorful label in the center. However, the label would often peel off and lose recognizablility.",
+    "rating": "xxxx",
+    "img": "glass.png",
+  },
+  {
+    "name": "The First Bottle",
+    "year": "1894",
+    "material": "Glass with a horseshoe stopper",
+    "fact": "Also known as the Hutchinson bottle, this was the first bottled product of Coca-Cola. When the stopper was removed, the seal made a 'pop' sound which coined the popular term, soda pop. ",
+    "rating": "xxxx",
+    "img": "firstGlass.png",
+  },
+    
+    
+    
+    
+    
+]
+
+
+
+window.addEventListener('load', ()=>{
+  init();
+})
+
+const init = function(){
+  const timeline = document.getElementById("timeline");
+
+  bottles.map(function(bottle){
+      const bottleItem = buildBottleItem(bottle);
+      timeline.innerHTML = timeline.innerHTML + bottleItem;
+  })
+}
+
+
+const buildBottleItem = function(bottle){
+  return `<div class="section">
+       <img src="${bottle.img}" alt="share a coke">
+       <div class="line"></div>
+       <div class="dot"></div>
+       <div class="year">${bottle.year}</div>
+      <div class="details">
+          <div class="one">${bottle.name}</div>
+          <div class="two">${bottle.material}</div>
+          <div class="three">${bottle.fact}</div>
+          <div class="three">${bottle.rating}</div>
+        </div>
+  </div>`
+
+}
+
+  // GSAP horizontal scrolling setup
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Horizontal scrolling animation
+  gsap.to("#container", {
+    x: () => -(document.getElementById("container").scrollWidth - document.documentElement.clientWidth),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#container",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true,
+      horizontal: true // Enable horizontal scrolling
+    }
+  });
+
+
+$("#container").on("scroll", function (e) {
+  horizontal = e.currentTarget.scrollLeft;
+  vertical = e.currentTarget.scrollTop;
+  });
+
+const sections = gsap.toArray('.container section')
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease:"none",
+  scrollTrigger:{
+    trigger: ".scrolling-wrapper",
+    pin:"true",
+    scrub: 1,
+    end:"+=3000",
+  }
+})
